987eee26a88d59d1c0a5ddb73cc8bc9886dd212f
ferueda <fe.rueda@outlook.com>
2020-11-30 22:38:21 -0300

    Create shared Button component

---
+++ src/components/shared/Button.js
@@ -0,0 +1,13 @@
+function Button({ children, type = 'button', onClick }) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className="text-gray-600 rounded-md bg-gray-200 border border-gray-300 px-10 py-1 text-xs cursor-default shadow active:bg-blue-400 active:text-white active:border-blue-500"
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
