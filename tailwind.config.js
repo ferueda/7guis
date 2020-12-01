0a9d0a815bb6609cea594cc00f2771eb37878bf1
ferueda <fe.rueda@outlook.com>
2020-11-30 22:37:53 -0300

    Update Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: ['./src/**/*.js', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      minWidth: {
+        200: '200px',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+      textColor: ['active'],
+      borderColor: ['active'],
+    },
   },
   plugins: [],
 };
