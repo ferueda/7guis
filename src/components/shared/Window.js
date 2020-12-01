ccc61b1945e6f0e3198033b08df6244a26ad24e2
ferueda <fe.rueda@outlook.com>
2020-12-01 18:24:47 -0300

    Update styles in Window.js

--- src/components/shared/Window.js
+++ src/components/shared/Window.js
@@ -1,6 +1,6 @@
 function Window({ children }) {
   return (
-    <div className="max-w-max bg-gray-50 border rounded-md border-gray-200 shadow-lg">
+    <div className="max-w-max bg-gray-100 border rounded-md border-gray-300 shadow-lg">
       {children}
     </div>
   );
