c77af54ef1f7c4c6637c9a86832cf6a2f808eb24
ferueda <fe.rueda@outlook.com>
2020-12-01 18:24:22 -0300

    Update styles in Button.js

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -3,7 +3,7 @@ function Button({ children, type = 'button', onClick }) {
     <button
       type={type}
       onClick={onClick}
-      className="text-gray-600 rounded-md bg-gray-200 border border-gray-300 px-10 py-1 text-xs cursor-default shadow active:bg-blue-400 active:text-white active:border-blue-500"
+      className="text-gray-600 rounded-md bg-white border border-gray-300 px-10 py-1 text-xs cursor-default shadow active:bg-blue-400 active:text-white active:border-blue-500"
     >
       {children}
     </button>
