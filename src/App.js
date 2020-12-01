4364466116fce369bbd08b3c7dabcae76a600d3b
ferueda <fe.rueda@outlook.com>
2020-12-01 18:25:37 -0300

    Add TempConverter to main view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Counter from './guis/Counter';
+import TempConverter from './guis/TempConverter';
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen w-full ">
+    <main className="flex flex-col items-center justify-evenly h-screen w-full ">
       <Counter />
+      <TempConverter />
     </main>
   );
 }
