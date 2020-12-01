35e16ca1c36acfcc7759105d1d6835e08e711229
ferueda <fe.rueda@outlook.com>
2020-12-01 18:24:33 -0300

    Update styles in Header.js

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,6 +1,6 @@
 function Header({ children }) {
   return (
-    <header className="flex bg-gray-200 text-gray-500 border-b rounded-t-md justify-center w-full px-1 text-base shadow">
+    <header className="flex bg-gray-300 text-gray-600 rounded-t-md justify-center w-full px-1 text-base shadow cursor-default">
       {children}
     </header>
   );
