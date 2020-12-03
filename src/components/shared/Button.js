0f482d60fe470e9fefc6747f260cff52ed704a91
ferueda <fe.rueda@outlook.com>
2020-12-02 22:45:50 -0300

    Add disable props and state to the Button component

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,9 +1,13 @@
-function Button({ children, type = 'button', onClick }) {
+function Button({ children, type = 'button', onClick, isDisabled = false }) {
+  console.log(isDisabled);
   return (
     <button
       type={type}
       onClick={onClick}
-      className="text-gray-600 rounded-md bg-white border border-gray-300 px-10 py-1 text-xs cursor-default shadow active:bg-blue-400 active:text-white active:border-blue-500"
+      disabled={isDisabled}
+      className={`text-gray-600 rounded-md bg-white border border-gray-300 px-10 py-1 text-xs cursor-default shadow ${
+        !isDisabled && 'active:bg-blue-400 active:text-white active:border-blue-500'
+      } ${isDisabled && 'cursor-not-allowed opacity-50'}`}
     >
       {children}
     </button>
