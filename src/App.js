b572a6655fba0a3261e3dde86a75a849a7753f23
ferueda <fe.rueda@outlook.com>
2020-12-02 22:47:35 -0300

    Update App.js
    Added title
    Added FlightBooker component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Counter from './guis/Counter';
 import TempConverter from './guis/TempConverter';
+import FlightBooker from './guis/FlightBooker';
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-evenly h-screen w-full ">
-      <Counter />
-      <TempConverter />
+    <main className="flex flex-col items-center h-screen w-full">
+      <h1 className="text-xl font-bold text-center mt-5 text-blue-500 hover:underline">
+        <a href="https://eugenkiss.github.io/7guis/tasks" target="_blank" rel="noreferrer">
+          7 GUIs Challenge
+        </a>
+      </h1>
+
+      <h3 className="text-center">Implementation using React and Tailwind CSS</h3>
+
+      <div className="flex flex-col items-center justify-evenly h-screen w-full">
+        <Counter />
+        <TempConverter />
+        <FlightBooker />
+      </div>
     </main>
   );
 }
