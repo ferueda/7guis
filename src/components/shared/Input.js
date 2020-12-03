adbc73025d2770075d6cc8701774b170a92ed8b5
ferueda <fe.rueda@outlook.com>
2020-12-02 22:46:21 -0300

    Update Input.js
    Added styles for disabled state and validation

--- src/components/shared/Input.js
+++ src/components/shared/Input.js
@@ -1,15 +1,25 @@
-function Input({ type = 'text', id, placeholder = '', name = '', onChange, value, isValid }) {
+function Input({
+  type = 'text',
+  id,
+  placeholder = '',
+  name = '',
+  onChange,
+  value,
+  isValid = true,
+  isDisabled = false,
+}) {
   return (
     <input
       id={id}
       name={name}
       type={type}
       placeholder={placeholder}
-      className={`rounded-md bg-white border border-gray-200 text-sm shadow py-1 pl-2 w-28 ${
+      className={`rounded-md bg-white border border-gray-200 text-sm shadow py-1 pl-2 w-full ${
         !isValid && 'bg-red-300 border-red-300'
-      }`}
+      } ${isDisabled && 'cursor-not-allowed bg-gray-100'}`}
       onChange={onChange}
       value={value}
+      disabled={isDisabled}
     />
   );
 }
