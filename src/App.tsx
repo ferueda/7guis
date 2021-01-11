a4de374020e7832182c5264fbc5679c155265d12
ferueda <fe.rueda@outlook.com>
2021-01-11 11:58:00 -0300

    Update styles to main

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,26 @@ import TempConverter from './guis/TempConverter';
 import FlightBooker from './guis/FlightBooker';
 import Timer from './guis/Timer';
 import Crud from './guis/Crud';
+import CircleDrawer from './guis/CircleDrawer';
 
 const App: React.FC = () => {
   return (
-    <main className="flex flex-col items-center h-screen w-full">
-      <h1 className="text-xl font-bold text-center mt-5 text-blue-500 hover:underline">
+    <main className="flex flex-col items-evenly w-full overflow-y-scroll p-6">
+      <h1 className="text-xl font-bold text-center mb-2 text-blue-500 hover:underline">
         <a href="https://eugenkiss.github.io/7guis/tasks" target="_blank" rel="noreferrer">
           7 GUIs Challenge
         </a>
       </h1>
 
-      <h3 className="text-center">Implementation using React, TypeScript and Tailwind CSS</h3>
+      <h3 className="text-center mb-4">Implementation using React, TypeScript and Tailwind CSS</h3>
 
-      <div className="flex flex-col items-center justify-evenly h-screen w-full">
+      <div className="flex flex-col items-center justify-between w-full">
         <Counter />
         <TempConverter />
         <FlightBooker />
         <Timer />
         <Crud />
+        <CircleDrawer />
       </div>
     </main>
   );
